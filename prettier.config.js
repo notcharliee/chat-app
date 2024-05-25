f2072c2f45baa4b04f40d8e81f5893d7d33c4b02
notcharliee <saunderscharlie@hotmail.com>
2024-05-25 15:40:50 +0100

    chore: setup import sorting

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,31 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  importOrder: [
+    "^(bun/(.*)$)|^(bun$)",
+    "<BUILTIN_MODULES>",
+    "",
+    "^~/styles/(.*)$",
+    "",
+    "^(next/(.*)$)|^(next$)",
+    "^(react/(.*)$)|^(react$)",
+    "",
+    "^~/hooks/(.*)$",
+    "",
+    "^~/components/(.*)$",
+    "",
+    "^~/lib/(.*)$",
+    "",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^~/types/(.*)$",
+    "",
+    "^[~/]",
+    "^[.]",
+  ],
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
   semi: false,
 }
 
